1121f294a04d9dc527b062032956bd2ba3529d4f
ogiroux <ogiroux@gmail.com>
2016-05-23 16:16:05 -0700

    Missing test update

--- test.cpp
+++ test.cpp
@@ -1,30 +1,30 @@
 /*
- 
- Copyright (c) 2014, NVIDIA Corporation
- All rights reserved.
- 
- Redistribution and use in source and binary forms, with or without modification,
- are permitted provided that the following conditions are met:
- 
- 1. Redistributions of source code must retain the above copyright notice, this
- list of conditions and the following disclaimer.
- 
- 2. Redistributions in binary form must reproduce the above copyright notice,
- this list of conditions and the following disclaimer in the documentation
- and/or other materials provided with the distribution.
- 
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
- OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- OF THE POSSIBILITY OF SUCH DAMAGE.
- 
- */
+
+Copyright (c) 2014, NVIDIA Corporation
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
 
 #ifdef WIN32
 #define _WIN32_WINNT 0x0601
@@ -43,7 +43,7 @@
 #include <algorithm>
 
 static constexpr int measure_count = 1 << 30;
-static constexpr int time_target_in_seconds = 10;
+static constexpr int time_target_in_seconds = 4;
 
 #if defined(__linux__) || defined(__APPLE__)
 #include <unistd.h>
@@ -69,13 +69,13 @@ double system_time_consumed(cpu_time start, cpu_time end) {
 #ifdef __linux__
 #include <sched.h>
 void set_affinity(std::uint64_t cpu) {
-    
+
     cpu_set_t cpuset;
     CPU_ZERO(&cpuset);
-    
+
     cpu %= sizeof(int) * 8;
     CPU_SET(cpu, &cpuset);
-    
+
     sched_setaffinity(0, sizeof(cpuset), &cpuset);
 }
 #endif
@@ -84,13 +84,13 @@ void set_affinity(std::uint64_t cpu) {
 #include <mach/thread_policy.h>
 #include <pthread.h>
 
-extern "C" kern_return_t thread_policy_set( thread_t                thread,
-                                           thread_policy_flavor_t  flavor,
-                                           thread_policy_t         policy_info,
-                                           mach_msg_type_number_t  count);
+extern "C" kern_return_t thread_policy_set(thread_t                thread,
+    thread_policy_flavor_t  flavor,
+    thread_policy_t         policy_info,
+    mach_msg_type_number_t  count);
 
 void set_affinity(std::uint64_t cpu) {
-    
+
     cpu %= sizeof(integer_t) * 8;
     integer_t count = (1 << cpu);
     thread_policy_set(pthread_mach_thread_np(pthread_self()), THREAD_AFFINITY_POLICY, (thread_policy_t)&count, 1);
@@ -99,7 +99,7 @@ void set_affinity(std::uint64_t cpu) {
 
 #ifdef WIN32
 static HANDLE self = GetCurrentProcess();
-typedef std::pair<FILETIME,FILETIME> cpu_time;
+typedef std::pair<FILETIME, FILETIME> cpu_time;
 cpu_time get_cpu_time() {
     cpu_time t;
     FILETIME ftime, fsys, fuser;
@@ -115,27 +115,27 @@ std::uint64_t make64(FILETIME ftime) {
     return make64(ftime.dwLowDateTime, ftime.dwHighDateTime);
 }
 double user_time_consumed(cpu_time start, cpu_time end) {
-    
+
     double nanoseconds_per_clock_tick = 100; //100-nanosecond intervals
     auto clock_ticks_elapsed = make64(end.second) - make64(start.second);
     return clock_ticks_elapsed * nanoseconds_per_clock_tick;
 }
 double system_time_consumed(cpu_time start, cpu_time end) {
-    
+
     double nanoseconds_per_clock_tick = 100; //100-nanosecond intervals
     auto clock_ticks_elapsed = make64(end.first) - make64(start.first);
     return clock_ticks_elapsed * nanoseconds_per_clock_tick;
 }
 void set_affinity(std::uint64_t cpu) {
-    
+
     cpu %= sizeof(std::uint64_t) * 8;
     SetThreadAffinityMask(GetCurrentThread(), int(1 << cpu));
 }
 #endif
 
-#ifdef MORE_LOCKS
+#ifdef WTF_LOCKS
 // On Mac, you can build this in your Git/WebKit directory like so:
-// xcrun clang++ -DMORE_LOCKS=1 -IPATH_TO_SYNCHRONIC/synchronic/include -IPATH_TO_SYNCHRONIC/synchronic -o LockSpeedTest2 Source/WTF/benchmarks/LockSpeedTest2.cpp -O3 -W -ISource/WTF -ISource/WTF/benchmarks -LWebKitBuild/Release -lWTF -framework Foundation -licucore -std=c++11 -fvisibility=hidden
+// xcrun clang++ -DWTF_LOCKS=1 -IPATH_TO_SYNCHRONIC/synchronic/include -IPATH_TO_SYNCHRONIC/synchronic -o LockSpeedTest2 Source/WTF/benchmarks/LockSpeedTest2.cpp -O3 -W -ISource/WTF -ISource/WTF/benchmarks -LWebKitBuild/Release -lWTF -framework Foundation -licucore -std=c++11 -fvisibility=hidden
 
 #include "config.h"
 
@@ -156,25 +156,25 @@ void set_affinity(std::uint64_t cpu) {
 #endif
 
 using my_clock = std::conditional<std::chrono::high_resolution_clock::is_steady,
-std::chrono::high_resolution_clock, std::chrono::steady_clock>::type;
+    std::chrono::high_resolution_clock, std::chrono::steady_clock>::type;
 
 template <class R>
-double compute_work_item_cost(R r)  {
-    
+double compute_work_item_cost(R r) {
+
     auto start = my_clock::now();
-    
+
     //perform work
     for (int i = 0; i < measure_count; ++i) r.discard(1);
-    
+
     auto end = my_clock::now();
-    
+
     return std::chrono::nanoseconds(end - start).count() / double(measure_count);
 }
 
 class run {
     std::atomic<bool> go, stop;
     std::atomic<int> running, iterations;
-    public :
+public:
     run() : go(false), stop(false), running(0), iterations(0) { }
     struct report {
         double wall_time, user_time, system_time;
@@ -185,7 +185,7 @@ class run {
         std::random_device d;
         for (int i = 0; i < threads; ++i) {
             auto s = d();
-            std::thread([&,f, i, s]() mutable {
+            std::thread([&, f, i, s]() mutable {
                 std::mt19937 r;
                 r.seed(s);
                 set_affinity(i);
@@ -219,13 +219,13 @@ class run {
         stop = true;
         while (running != 0) std::this_thread::yield();
         std::this_thread::sleep_for(std::chrono::seconds(1));
-        
+
         report r;
         r.wall_time = double(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
         r.user_time = user_time_consumed(cpu_start, cpu_end);
         r.system_time = system_time_consumed(cpu_start, cpu_end);
         r.steps = (it2 - it1);
-        
+
         return r;
     }
 };
@@ -234,70 +234,73 @@ template <class F>
 double do_run(std::ostream& csv, std::string const& what, int threads, F f, int count, double cost)
 {
     std::cout << "Measuring " << what << " (" << threads << "T, " << time_target_in_seconds
-    << "s, step = " << count << " x " << cost << " ns)" << std::endl;
-    
+        << "s, step = " << count << " x " << cost << " ns)" << std::endl;
+
     auto expected = count * cost;
     auto r = run().time(threads, f, std::uint64_t(time_target_in_seconds * 1E9 / expected));
     std::cout << std::right << std::setfill(' ') << std::setw(20) <<
-    "total progress : " << r.steps << " steps in " << r.wall_time << "ns" << std::endl;
-    
+        "total progress : " << r.steps << " steps in " << r.wall_time << "ns" << std::endl;
+
     auto wall_time = r.wall_time / r.steps;
     auto user_time = r.user_time / r.steps;
     auto system_time = r.system_time / r.steps;
     auto cpu_time = user_time + system_time;
     /*
-     std::cout << std::right << std::setfill(' ') << std::setw(20) <<
-     "expected : " << expected << " ns/step" << std::endl;
-     
-     std::cout << std::right << std::setfill(' ') << std::setw(20) <<
-     "real : " << wall_time << " ns/step (" << wall_time / expected * 100 << "%)" << std::endl;
-     
-     std::cout << std::right << std::setfill(' ') << std::setw(20) <<
-     "cpu : " << cpu_time << " ns/step (" << cpu_time / expected * 100 << "%)" << std::endl;
-     */
     std::cout << std::right << std::setfill(' ') << std::setw(20) <<
-    "[user : " << user_time / cpu_time * 100 << "%]" << std::endl;
+    "expected : " << expected << " ns/step" << std::endl;
+
     std::cout << std::right << std::setfill(' ') << std::setw(20) <<
-    "[system : " << system_time / cpu_time * 100 << "%]" << std::endl;
-    
+    "real : " << wall_time << " ns/step (" << wall_time / expected * 100 << "%)" << std::endl;
+
+    std::cout << std::right << std::setfill(' ') << std::setw(20) <<
+    "cpu : " << cpu_time << " ns/step (" << cpu_time / expected * 100 << "%)" << std::endl;
+    */
+    std::cout << std::right << std::setfill(' ') << std::setw(20) <<
+        "[user : " << user_time / cpu_time * 100 << "%]" << std::endl;
+    std::cout << std::right << std::setfill(' ') << std::setw(20) <<
+        "[system : " << system_time / cpu_time * 100 << "%]" << std::endl;
+
     std::cout << std::endl;
-    
+
     csv << "\"" << what << "\"," << threads << ',' << r.steps << ',' << r.wall_time << ',' << expected << ','
-    << wall_time / expected << ','  << cpu_time / expected << ','  << user_time / cpu_time << ','  << system_time / cpu_time << std::endl;
-    
+        << wall_time / expected << ',' << cpu_time / expected << ',' << user_time / cpu_time << ',' << system_time / cpu_time << std::endl;
+
     return wall_time / count;
 }
 
 int main(int, const char *[]) {
-    
-#ifdef MORE_LOCKS
+
+#ifdef WTF_LOCKS
     WTF::initializeThreading();
 #endif
-    
+
     std::ostringstream nullstream;
     std::ofstream csv("output.csv");
-    if(!csv) {
+    if (!csv) {
         std::cout << "ERROR: could not open the output file." << std::endl;
         return 0;
     }
-    
+
     csv << "name, threads, steps, time, expected, real/expected, cpu/expected, user/cpu, system/cpu" << std::endl;
-    
+
     std::mt19937 r;
     std::mutex m1;
     typedef ttas_mutex test_mutex_1;
     test_mutex_1 m2;
-    
-#ifdef MORE_LOCKS
+
+#ifdef WTF_LOCKS
     typedef WTF::Lock test_mutex_2;
-    test_mutex_2 m3;
+#else
+    typedef webkit_mutex test_mutex_2;
 #endif
-    
+
+    test_mutex_2 m3;
+
     auto const N = std::thread::hardware_concurrency();
     assert(N <= 1024);
-    
+
     // CONTROL FOR 1-THREAD
-    
+
     std::cout << "Warming up...\r" << std::flush;
     compute_work_item_cost(r);
     auto cost = compute_work_item_cost(r);
@@ -307,71 +310,66 @@ int main(int, const char *[]) {
     }, target_count, cost);
     std::cout << "Adjusting cost to " << cost << " ns/iteration (targeting " << target_count << " iterations/step).\n";
     std::cout << std::endl;
-    
+
     // SINGLE THREADED
-    
-    do_run(csv, "std::mutex single-threaded", 1, [=,&m1](int, std::mt19937&) mutable {
+
+    do_run(csv, "std::mutex single-threaded", 1, [=, &m1](int, std::mt19937&) mutable {
         { std::unique_lock<std::mutex>(m1); }
         for (int i = 0; i < target_count; ++i) r.discard(1);
     }, target_count, cost);
-    do_run(csv, "ttas_mutex single-threaded", 1, [=,&m2](int, std::mt19937&) mutable {
+    do_run(csv, "ttas_mutex single-threaded", 1, [=, &m2](int, std::mt19937&) mutable {
         { std::unique_lock<test_mutex_1>(m2); }
         for (int i = 0; i < target_count; ++i) r.discard(1);
     }, target_count, cost);
-#ifdef MORE_LOCKS
-    do_run(csv, "wtf_mutex single-threaded", 1, [=,&m2](int, std::mt19937&) mutable {
+    do_run(csv, "webkit_mutex single-threaded", 1, [=, &m2](int, std::mt19937&) mutable {
         { std::unique_lock<test_mutex_2>(m3); }
         for (int i = 0; i < target_count; ++i) r.discard(1);
     }, target_count, cost);
-#endif
     // CONTROL FOR N-THREAD
-    
+
     auto cost_n = do_run(nullstream, "CONTROL for uncontended N-thread", N, [=](int, std::mt19937&) mutable {
         for (int i = 0; i < target_count; ++i) r.discard(1);
     }, target_count, cost);
-    if(cost_n < cost)
+    if (cost_n < cost)
         std::cout << "NOTE: Based purely on these numbers, your system appears to have hyper-threads enabled.\n";
     cost = cost_n;
     std::cout << "Adjusting cost to " << cost << " ns/iteration (targeting " << target_count << " iterations/step).\n";
     std::cout << std::endl;
-    
+
     //
-    
+
     std::mutex m1N[1024];
     test_mutex_1 m2N[1024];
-#ifdef MORE_LOCKS
     test_mutex_2 m3N[1024];
-#endif
-    
+
     // NO CONTENTION
-    
-    do_run(csv, "std::mutex no contention", N, [=,&m1N](int i, std::mt19937&) mutable {
+
+    do_run(csv, "std::mutex no contention", N, [=, &m1N](int i, std::mt19937&) mutable {
         auto& m = m1N[i];
         { std::unique_lock<std::mutex> l(m); }
         for (int i = 0; i < target_count; ++i) r.discard(1);
     }, target_count, cost);
-    do_run(csv, "ttas_mutex no contention", N, [=,&m2N](int i, std::mt19937&) mutable {
+    do_run(csv, "ttas_mutex no contention", N, [=, &m2N](int i, std::mt19937&) mutable {
         auto& m = m2N[i];
         { std::unique_lock<test_mutex_1> l(m); }
         for (int i = 0; i < target_count; ++i) r.discard(1);
     }, target_count, cost);
-#ifdef MORE_LOCKS
-    do_run(csv, "wtf_mutex no contention", N, [=,&m3N](int i, std::mt19937&) mutable {
+    do_run(csv, "webkit_mutex no contention", N, [=, &m3N](int i, std::mt19937&) mutable {
         auto& m = m3N[i];
         { std::unique_lock<test_mutex_2> l(m); }
         for (int i = 0; i < target_count; ++i) r.discard(1);
     }, target_count, cost);
-#endif
+
     // LOW-P CONTENTION
-    
+
     {
         std::random_device d;
-        if(d.entropy() == 0)
+        if (d.entropy() == 0)
             std::cout << "NOTE: the system randomness source claims to have no entropy, low-p tests may not operate correctly." << std::endl;
         std::cout << std::endl;
     }
     auto mask = (4 << std::ilogb(N)) - 1;
-    do_run(csv, "std::mutex rare contention", N, [=,&m1N](int, std::mt19937& dr) mutable {
+    do_run(csv, "std::mutex rare contention", N, [=, &m1N](int, std::mt19937& dr) mutable {
         auto& m = m1N[dr() & mask];
         { std::unique_lock<std::mutex> l(m); }
         for (int i = 0; i < target_count; ++i) r.discard(1);
@@ -381,15 +379,14 @@ int main(int, const char *[]) {
         { std::unique_lock<test_mutex_1> l(m); }
         for (int i = 0; i < target_count; ++i) r.discard(1);
     }, target_count, cost);
-#ifdef MORE_LOCKS
-    do_run(csv, "wtf_mutex rare contention", N, [=, &m3N](int, std::mt19937& dr) mutable {
+    do_run(csv, "webkit_mutex rare contention", N, [=, &m3N](int, std::mt19937& dr) mutable {
         auto& m = m3N[dr() & mask];
         { std::unique_lock<test_mutex_2> l(m); }
         for (int i = 0; i < target_count; ++i) r.discard(1);
     }, target_count, cost);
-#endif
+
     // SHORTEST
-    
+
     auto shortest_count = 1;
     do_run(csv, "std::mutex shortest sections", N, [=, &m1](int, std::mt19937&) mutable {
         std::unique_lock<std::mutex> l(m1);
@@ -399,14 +396,13 @@ int main(int, const char *[]) {
         std::unique_lock<test_mutex_1> l(m2);
         r.discard(1);
     }, shortest_count, cost);
-#ifdef MORE_LOCKS
-    do_run(csv, "wtf_mutex shortest sections", N, [=, &m3](int, std::mt19937&) mutable {
+    do_run(csv, "webkit_mutex shortest sections", N, [=, &m3](int, std::mt19937&) mutable {
         std::unique_lock<test_mutex_2> l(m3);
         r.discard(1);
     }, shortest_count, cost);
-#endif
+
     // SHORT
-    
+
     auto short_count = int(2E2 / cost);
     do_run(csv, "std::mutex short sections", N, [=, &m1](int, std::mt19937&) mutable {
         std::unique_lock<std::mutex> l(m1);
@@ -416,14 +412,13 @@ int main(int, const char *[]) {
         std::unique_lock<test_mutex_1> l(m2);
         for (int i = 0; i < short_count; ++i) r.discard(1);
     }, short_count, cost);
-#ifdef MORE_LOCKS
-    do_run(csv, "wtf_mutex short sections", N, [=, &m3](int, std::mt19937&) mutable {
+    do_run(csv, "webkit_mutex short sections", N, [=, &m3](int, std::mt19937&) mutable {
         std::unique_lock<test_mutex_2> l(m3);
         for (int i = 0; i < short_count; ++i) r.discard(1);
     }, short_count, cost);
-#endif
+
     // LONG
-    
+
     auto long_count = int(1E7 / cost);
     do_run(csv, "std::mutex long sections", N, [=, &m1](int, std::mt19937&) mutable {
         std::unique_lock<std::mutex> l(m1);
@@ -433,13 +428,12 @@ int main(int, const char *[]) {
         std::unique_lock<test_mutex_1> l(m2);
         for (int i = 0; i < long_count; ++i) r.discard(1);
     }, long_count, cost);
-#ifdef MORE_LOCKS
-    do_run(csv, "wtf_mutex long sections", N, [=, &m3](int, std::mt19937&) mutable {
+    do_run(csv, "webkit_mutex long sections", N, [=, &m3](int, std::mt19937&) mutable {
         std::unique_lock<test_mutex_2> l(m3);
         for (int i = 0; i < long_count; ++i) r.discard(1);
     }, long_count, cost);
-#endif
+
     //
-    
+
     return 0;
 }
